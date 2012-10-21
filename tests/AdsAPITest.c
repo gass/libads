f143d1c376aec6bbba20e7e1e826e1eeefe2d6a9
Luis Matos <gass@otiliamatos.ath.cx>
2012-10-21 23:58:19 +0100

    AdsAPITest: removed unused variable

--- tests/AdsAPITest.c
+++ tests/AdsAPITest.c
@@ -1,20 +1,16 @@
 #include <stdio.h>
 
-#ifdef __linux__
-#include <unistd.h>
-#endif
-
 #include <AdsAPI.h>
 #include <AdsDEF.h>
 
 
 int main(int argc, char **argv)
 {
-    long nErr, nPort;
+    long nErr;
     AmsAddr addr;
     PAmsAddr pAddr = &addr;
     
-    nPort = AdsPortOpen();
+    AdsPortOpen();
     nErr = AdsGetLocalAddress(pAddr);
     
     if (nErr) printf("Error: AdsGetLocalAddress %ld\n", nErr);    
