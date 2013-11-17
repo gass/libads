480849ef964d01c806c6303f0ddd35d84bf1fd5c
Luis Matos <luis.matos@agicore.pt>
2013-11-17 21:50:15 +0000

    updated license headers

--- src/AdsDEF.h
+++ src/AdsDEF.h
@@ -1,20 +1,27 @@
 /*
-    libads is an implementation of the Beckhoff's ADS protocol.
-    
-    (C) Luis Matos (gass@otiliamatos.ath.cx) 2009.
+ Implementation of BECKHOFF's ADS protocol.
+ ADS = Automation Device Specification
+ Implemented according to specifications given in TwinCAT Information System
+ May 2011.
+ TwinCAT, ADS and maybe other terms used herein are registered trademarks of
+ BECKHOFF Company. www.beckhoff.de
 
-    libads is free software: you can redistribute it and/or modify
-    it under the terms of the Lesser GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
+ Copyright (C) Luis Matos (gass@otiliamatos.ath.cx) 2009.
+ Copyright (C) Gerhard Schiller (gerhard.schiller@gmail.com) 2013.
 
-    libads is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    Lesser GNU General Public License for more details.
+ This file is part of libads.  
+ Libads is free software: you can redistribute it and/or modify
+ it under the terms of the Lesser GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
 
-    You should have received a copy of the Lesser GNU General Public License
-    along with libads.  If not, see <http://www.gnu.org/licenses/>.
+ libads is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ Lesser GNU General Public License for more details.
+
+ You should have received a copy of the Lesser GNU General Public License
+ along with libads.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 #ifdef __cplusplus
--- tests/AdsAPITest.c
+++ tests/AdsAPITest.c
@@ -1,3 +1,28 @@
+/*
+ Implementation of BECKHOFF's ADS protocol.
+ ADS = Automation Device Specification
+ Implemented according to specifications given in TwinCAT Information System
+ May 2011.
+ TwinCAT, ADS and maybe other terms used herein are registered trademarks of
+ BECKHOFF Company. www.beckhoff.de
+
+ Copyright (C) Luis Matos (gass@otiliamatos.ath.cx) 2009.
+
+ This file is part of libads.  
+ Libads is free software: you can redistribute it and/or modify
+ it under the terms of the Lesser GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ libads is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ Lesser GNU General Public License for more details.
+
+ You should have received a copy of the Lesser GNU General Public License
+ along with libads.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #include <stdio.h>
 
 #include <AdsAPI.h>
